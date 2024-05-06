02e1e01490b0a1de1626f4c3a46efe1f9e339f6c
LanceaKing <bomb_k@163.com>
2024-05-06 09:56:54 +0800

    fix requests default headers

--- requests_go/tls_client/client.py
+++ requests_go/tls_client/client.py
@@ -4,16 +4,16 @@ from sys import platform
 from platform import machine
 
 if platform == 'darwin':
-    file_ext = '-arm64.dylib' if machine() == "arm64" else '-x86.dylib'
+    file_ext = '-darwin_arm64.dylib' if machine() == "arm64" else '-darwin_amd64.dylib'
 elif platform in ('win32', 'cygwin'):
-    file_ext = '-win64.dll' if 8 == ctypes.sizeof(ctypes.c_voidp) else '-win32.dll'
+    file_ext = '-windows_amd64.dll' if 8 == ctypes.sizeof(ctypes.c_voidp) else '-windows_386.dll'
 else:
     if machine() == "aarch64":
-        file_ext = '-arm64.so'
-    elif "x86" in machine():
-        file_ext = '-x86.so'
+        file_ext = '-linux_arm64.so'
+    elif machine() == "x86_64":
+        file_ext = '-linux_amd64.so'
     else:
-        file_ext = '-amd64.so'
+        file_ext = '-linux_386.so'
 
 root_dir = os.path.abspath(os.path.dirname(__file__))
 library = ctypes.cdll.LoadLibrary(f'{root_dir}/dependencies/requests-go{file_ext}')
